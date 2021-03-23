7573e6612a4e47e6997b20e9dff80c1c784f7622
HP <HP@NagaChaitanya>
2021-03-23 09:31:02 +0530

    data in RBGTech Admin demo

---
+++ rbgtechdemo/src/rbgtechdemo/RbgTechDemo.java
@@ -0,0 +1,18 @@
+package rbgtechdemo;
+
+public class RbgTechDemo {
+
+	public static void main(String[] args) 
+	{
+	String data="yes";
+	if( data=="yes")
+	{
+	System.out.println("print the data in your work book");	
+	}
+	else
+	{
+		System.out.println("not to print any data in work book");
+	}
+	}
+
+}
