152e6cb8b5e777aa01da5a677831ca91585a25e9
HP <HP@NagaChaitanya>
2021-03-23 10:31:07 +0530

    integer comparison demo

---
+++ rbgtechdemo/src/rbgtechdemo/IntegerDemo.java
@@ -0,0 +1,19 @@
+package rbgtechdemo;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		Integer num1=50;
+		Integer num2=50;
+		if(num1==num2)
+		{
+			System.out.println("both are equal");
+		}
+		else
+		{
+			System.out.println("both are not equal");
+		}
+
+	}
+
+}
